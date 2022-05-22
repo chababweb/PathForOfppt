1b896ced1b61eea5cae7d26b20f3401042f159da
Abdallah Ismaili <89218851+AbdallahIsmaili@users.noreply.github.com>
2022-05-22 22:21:00 +0100

    update

--- index.php
+++ index.php
@@ -20,29 +20,18 @@
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
     <!-- Title: -->
-    <title>index</title>
+    <title>My Path In Ofppt</title>
 </head>
 <body>
 
     <!-- Navigation: -->
-    <nav class="navbar navbar-expand-lg navbar-dark py-3 bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark py-4 bg-dark">
         <div class="container">
             <a class="navbar-brand" href="index.php">
 
-            <!-- logo -->
-                <img src="images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-                <span class="d-none d-sm-inline">
+            <!-- Website Logo or user Profile logo -->
 
-            <!-- Php code to test if a user is logged in or not -->
-                    <?php
-                        if(isset($_SESSION['user'])){
-                            echo "Welcome ". $_SESSION['user'];
-                        }
-                        else{
-                            echo "Login";
-                        }
-                    ?>
-                </span>
+                <img src="images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
 
             </a>
 
@@ -52,34 +41,71 @@
             </button>
 
         <!-- Links -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+                <ul class="nav navbar-nav navbar-right">
+
+                    <!-- Checking if current page is index.php -->
+                    <?php
+                        if(basename($_SERVER['PHP_SELF']) != 'index.php'){
+                            echo '<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>';
+                        }
+                        else{
+                            echo '';
+                        }
+                    ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
+                        <a class="nav-link" href="about.php">My Path</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
+                        <a class="nav-link" href="about.php">Education</a>
                     </li>
+                    
+                    <!-- Mega menu here -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Courses
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="about.php">Courses</a>
+                    </li> -->
+
                     <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
+                        <a class="nav-link" href="contact.php">Exercises</a>
                     </li>
+
+                    <!-- Link to add post page here -->
+                    <?php
+                        if(isset($_SESSION['user'])){
+                            echo '<li class="nav-item"><a class="nav-link" href="add-post.php">Add Post</a></li>';
+                        }
+                        else{
+                            echo '<li class="nav-item"><a class="nav-link" href="registration.php">Add post now</a></li>';
+                        }
+
+                    ?>
 
                     <!-- Check if user is logged in or not -->
                     <?php
                         if(isset($_SESSION['user'])){
                             echo '<li class="nav-item">
-                                    <a class="nav-link" href="logout.php">Logout</a>
+                                    <a class="nav-link active" href="logout.php">'.$_SESSION['user'].'</a>
                                 </li>';
                         }
                         else{
                             echo '<li class="nav-item">
-                                    <a class="nav-link" href="login.php">Login now</a>
+                                    <a class="nav-link active" href="login.php">Login now</a>
                                 </li>';
                         }
 
-                    ?>
+                        ?>
                 </ul>
-            </div>
         </div>
     </nav>
 
@@ -87,10 +113,9 @@
     <a href="login.php">login</a>
     <a href="registration.php">registration</a> -->
 
-
     <!-- Scripts: -->
-    <script src="scriptsJs/jquery-3.3.1.min.js"></script>
-    <script src="scriptsJs/bootstrap.min.js"></script>
+    <script src="scriptsJs/jquery-3.6.0.js"></script>
+    <script src="scriptsJs/bootstrap.bundle.js"></script>
     <script src="scriptsJs/script.js"></script>
     <script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
 
