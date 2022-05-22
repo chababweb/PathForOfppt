7b35355e36a2592e1f6136592d043b0d55845f3a
team <105976811+chababweb@users.noreply.github.com>
2022-05-22 13:47:06 +0100

    Add files via upload

---
+++ BASE_PRO/OFPPT_PRO.sql
@@ -0,0 +1,125 @@
+CREATE DATABASE OFPPT_PRO
+USE OFPPT_PRO
+CREATE TABLE profil
+(Email VARCHAR(40) PRIMARY KEY,
+Pass VARCHAR(40),
+TypeP VARCHAR(20)
+)
+CREATE TABLE Formateur
+(CIN_For VARCHAR(20) PRIMARY KEY,
+Nom_For VARCHAR(20),
+Prenom_For VARCHAR(20),
+DateNaiss DATE,
+Email VARCHAR(40),
+CONSTRAINT fk_1 FOREIGN KEY (Email) REFERENCES profil(Email)
+)
+CREATE TABLE Stagiaire
+(CIN_STG VARCHAR(20) PRIMARY KEY,
+Nom_STG VARCHAR(20),
+Prenom_STG VARCHAR(20),
+DateNaiss DATE,
+Email VARCHAR(40),
+id_Group INT,
+CONSTRAINT fk_2 FOREIGN KEY (Email) REFERENCES profil(Email),
+CONSTRAINT fk_6 FOREIGN KEY (id_Group) REFERENCES Group_(id_Group)
+)
+DROP TABLE stagiaire
+CREATE TABLE Filiere
+(Code_Fil VARCHAR(20) PRIMARY KEY,
+Nom_Fil VARCHAR(20),
+Description TEXT 
+)
+CREATE TABLE Gestion_Filiere
+(Code_Fil VARCHAR(20),
+CIN_For VARCHAR(20),
+CONSTRAINT fk_3 FOREIGN KEY (Code_Fil) REFERENCES Filiere(Code_Fil),
+CONSTRAINT fk_4 FOREIGN KEY (CIN_For) REFERENCES Formateur(CIN_For),
+PRIMARY KEY(CIN_For,Code_Fil)
+)
+CREATE TABLE Group_
+(id_Group INT PRIMARY KEY,
+Nom_Group VARCHAR(20),
+Nbr_STG INT,
+Date_Debut DATE,
+Date_Fin DATE,
+Code_Fil VARCHAR(20),
+CONSTRAINT fk_5 FOREIGN KEY (Code_Fil) REFERENCES Filiere(Code_Fil)
+)
+CREATE TABLE Responsabiliter_Group
+(id_Group INT,
+CIN_For VARCHAR(20),
+CONSTRAINT fk_7 FOREIGN KEY (id_Group) REFERENCES Group_(id_Group),
+CONSTRAINT fk_8 FOREIGN KEY (CIN_For) REFERENCES Formateur(CIN_For),
+PRIMARY KEY(CIN_For,id_Group)
+)
+CREATE TABLE Post
+(id_Post INT PRIMARY KEY AUTO_INCREMENT,
+contenu TEXT,
+Email VARCHAR(40),
+CONSTRAINT fk_9 FOREIGN KEY (Email) REFERENCES profil(Email)
+)
+CREATE TABLE Commentaire
+(id_Post INT,
+Email VARCHAR(40),
+CONSTRAINT fk_10 FOREIGN KEY (id_Post) REFERENCES Post(id_Post),
+CONSTRAINT fk_11 FOREIGN KEY (Email) REFERENCES Profil(Email),
+PRIMARY KEY(id_Post,Email)
+)
+CREATE TABLE Module
+(id_Mod INT PRIMARY KEY,
+Nom_Mod VARCHAR(30),
+Code_Fil VARCHAR(20),
+CIN_For VARCHAR(20),
+CONSTRAINT fk_12 FOREIGN KEY (Code_Fil) REFERENCES Filiere(Code_Fil),
+CONSTRAINT fk_13 FOREIGN KEY (CIN_For) REFERENCES Formateur(CIN_For)
+)
+CREATE TABLE Cour
+(id_Cour INT PRIMARY KEY,
+Nom_Cour VARCHAR(20),
+contenu TEXT,
+id_Mod INT,
+CONSTRAINT fk_14 FOREIGN KEY (id_Mod) REFERENCES Module(id_Mod)
+)
+CREATE TABLE Tp
+(id_tp INT PRIMARY KEY,
+Nom_tp VARCHAR(20),
+contenu TEXT,
+id_Mod INT,
+CONSTRAINT fk_15 FOREIGN KEY (id_Mod) REFERENCES Module(id_Mod)
+)
+CREATE TABLE Exam
+(id_Exam INT PRIMARY KEY AUTO_INCREMENT,
+Num_Exam INT,
+DatePasse DATE,
+id_Mod INT,
+CONSTRAINT fk_16 FOREIGN KEY (id_Mod) REFERENCES Module(id_Mod)
+)
+CREATE TABLE Note
+(
+id_Exam INT,
+CIN_STG VARCHAR(20),
+Note FLOAT,
+CONSTRAINT fk_17 FOREIGN KEY (id_Exam) REFERENCES Exam(id_Exam),
+CONSTRAINT fk_18 FOREIGN KEY (CIN_STG) REFERENCES Stagiaire(CIN_STG),
+PRIMARY KEY(id_Exam,CIN_STG)
+)
+
+
+CREATE USER 'Fire_Knight'@'localhost' IDENTIFIED BY 'Viserion'
+GRANT ALL ON OFPPT_PRO.* TO 'Fire_Knight'@'localhost'
+CREATE USER 'Water_Knight'@'localhost' IDENTIFIED BY 'Knucker'
+GRANT ALL ON OFPPT_PRO.* TO 'Water_Knight'@'localhost'
+CREATE USER 'Air_Knight'@'localhost' IDENTIFIED BY 'Saphira'
+GRANT ALL ON OFPPT_PRO.* TO 'Air_Knight'@'localhost'
+CREATE USER 'Thunder_Knight'@'localhost' IDENTIFIED BY 'Balerion'
+GRANT ALL ON OFPPT_PRO.* TO 'Thunder_Knight'@'localhost'
+
+
+
+
+
+
+
+
+
+
